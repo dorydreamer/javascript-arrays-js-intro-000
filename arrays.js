5879a9d00280f69925e8caa401bd384425bd82e2
Doris Le <dorisle@sandiego.edu>
2020-01-14 22:13:59 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,12 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+
+var list = [1, 2, 3]
+function addElementToBeginningOfArray() {
+[0, 1]
+}
+
+var stuff = ["dog", "cat", "mouse"]
+function destructivelyAddElementToBeginningOfArray() {
+  stuff.unshift("fish")
+}
+return stuff
