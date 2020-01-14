75700f5336d088806395d4c31f9b555d5591cb0a
Doris Le <dorisle@sandiego.edu>
2020-01-14 22:15:45 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,6 +1,3 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
-function addElementToBeginningOfArray(array, element) {
-  [element, ...array]
-}
-return array
+function addElementToBeginningOfArray(array, element) {}
